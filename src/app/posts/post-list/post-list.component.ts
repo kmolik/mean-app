6df5fd7002adf896b1b06d124f4df901c34a3146
Konrad Molik <konrad.molik@gmail.com>
2019-04-18 11:36:40 +0200

    before mongo DB
    angular on //localhost:4200/posts/api
    server on //localhost:3000/posts/api

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -23,7 +23,7 @@ private postsSub: Subscription;
 constructor(public postsService: PostsService) {}
 
 ngOnInit(){
-  this.posts = this.postsService.getPosts();
+  this.postsService.getPosts();
   this.postsSub = this.postsService.getPostUpdateListener()
   .subscribe((posts: Post[]) => {
     this.posts = posts;
